88c05c023e8c610ad1322ecc996f6b7396077811
Samuel.mburu@pycs.co.ke <Samuel.mburu@pycs.co.ke>
2020-02-23 13:44:39 +0300

    Import swagger

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,15 @@ package com.example.demo;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
+@Configuration
+@Import(SwaggerConfig.class)
+@EnableAutoConfiguration
 @SpringBootApplication
 public class DemoApplication {
 
